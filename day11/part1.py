38f365c8b196bb7fcd97ce2a44174494114b8710
Connie <c.amer@icloud.com>
2024-12-15 13:45:21 +0000

    part 1 day 11

---
+++ day11/part1.py
@@ -0,0 +1,29 @@
+def read_file(file_path):
+    with open(file_path, 'r') as f:
+        return f.read().split()
+    
+file_path = 'input.txt'
+data = read_file(file_path=file_path)
+
+def blink(stones):
+    finalStones = []
+    for stone in stones:
+        if stone == "0":
+            finalStones.append("1")
+        elif len(stone) % 2 == 0: # even
+            half = len(stone) // 2
+
+            left = int(stone[:half])
+            right = int(stone[half:])
+
+            finalStones.append(str(left))
+            finalStones.append(str(right))
+        else:
+            finalStones.append(str((int(stone)*2024)))
+    
+    return finalStones
+
+for _ in range(0, 25):
+    data = blink(data)
+
+print(len(data))
